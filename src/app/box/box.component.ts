2eebb6209f3638ebf6e8b41c919a542988055fb2
Ahmad Sharif <ahmad.sharif@tuni.fi>
2022-04-08 13:50:07 +0300

    progress bar

--- src/app/box/box.component.ts
+++ src/app/box/box.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoxComponent implements OnInit {
 
+  initial_percentage: any = 25;
+  block_list = [1, 2];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changePercentage(event:any, value:any):void {
+      event.preventDefault();
+      this.initial_percentage = this.initial_percentage + value;
+      if(this.initial_percentage > 100) {
+        this.initial_percentage = 100;
+      }
+      if(this.initial_percentage < 1) {
+        this.initial_percentage = 0;
+      }
   }
 
 }
